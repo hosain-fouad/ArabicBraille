061d61b7896c940a8980ff8f9b4444af2c729f63
hosain-fathelbab <hosain.fathelbab@gmail.com>
2015-04-02 19:20:05 -0700

    cleaning

--- src/main/java/controller/TranslationService.java
+++ src/main/java/controller/TranslationService.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import tables.Grade1Map;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import static java.net.URLDecoder.decode;
@@ -11,67 +12,28 @@ import static java.net.URLDecoder.decode;
 @RestController
 public class TranslationService {
 
-
-    @RequestMapping(value="/translate", produces = "text/html; charset=UTF-8")
-    public @ResponseBody String translate (@RequestParam(value="input") String input) throws UnsupportedEncodingException {
-        String result = decode(input, "ASCII");
-        HashMap<Character, Character> table = new HashMap<Character, Character>();
-        table.put('ي','⠊');
-        table.put('و','⠺');
-        table.put('ه','⠓');
-        table.put('ن','⠝');
-        table.put('م','⠍');
-        table.put('ل','⠇');
-        table.put('ك','⠅');
-        table.put('ق','⠟');
-        table.put('ف','⠋');
-        table.put('غ','⠣');
-        table.put('ع','⠷');
-        table.put('ظ','⠿');
-        table.put('ط','⠾');
-        table.put('ض','⠫');
-        table.put('ص','⠯');
-        table.put('ش','⠩');
-        table.put('س','⠎');
-        table.put('ز' ,'⠵');
-        table.put('ر','⠗');
-        table.put('ذ','⠮');
-        table.put('د','⠙');
-        table.put('خ','⠭');
-        table.put('ح','⠱');
-        table.put('ج','⠚');
-        table.put('ث','⠹');
-        table.put('ت','⠞');
-        table.put('ب','⠃');
-        table.put('ا','⠁');
-        table.put('َ','⠂');
-        table.put('ِ','⠑');
-        table.put('ُ','⠥');
-        table.put('ً','⠆');
-        table.put('ٍ','⠔');
-        table.put('ٌ','⠢');
-        table.put('ّ','⠠');
-        table.put('ْ','⠒');
-        table.put('.','⠲');
-        table.put('،','⠐');
-
-
+    @RequestMapping(value = "/translate", produces = "text/html; charset=UTF-8")
+    public
+    @ResponseBody
+    String translate(@RequestParam(value = "input") String input) throws UnsupportedEncodingException {
+        HashMap<Character, Character> table = Grade1Map.table;
         StringBuffer sb = new StringBuffer();
 
-        for (char c : input.toCharArray()){
-            if(table.containsKey(c)){
+        for (char c : input.toCharArray()) {
+            if (table.containsKey(c)) {
                 sb.append(table.get(c));
-            }
-            else{
+            } else {
                 sb.append(c);
             }
         }
 
-    	return sb.toString();
+        return sb.toString();
     }
 
-    @RequestMapping(value="/translateEncoded", produces = "text/html; charset=UTF-8")
-    public @ResponseBody String translateEnocoded (@RequestParam(value="input") String input) throws UnsupportedEncodingException {
+    @RequestMapping(value = "/translateEncoded", produces = "text/html; charset=UTF-8")
+    public
+    @ResponseBody
+    String translateEnocoded(@RequestParam(value = "input") String input) throws UnsupportedEncodingException {
         input = decode(input, "ASCII");
         return translate(input);
     }
---
+++ src/main/java/tables/Grade1Map.java
@@ -0,0 +1,51 @@
+package tables;
+
+import java.util.HashMap;
+
+/**
+ * Created by hosainfathelbab on 4/2/15.
+ */
+public class Grade1Map {
+
+    public static HashMap<Character, Character> table = new HashMap<Character, Character>();
+    static{
+        table.put('ي','⠊');
+        table.put('و','⠺');
+        table.put('ه','⠓');
+        table.put('ن','⠝');
+        table.put('م','⠍');
+        table.put('ل','⠇');
+        table.put('ك','⠅');
+        table.put('ق','⠟');
+        table.put('ف','⠋');
+        table.put('غ','⠣');
+        table.put('ع','⠷');
+        table.put('ظ','⠿');
+        table.put('ط','⠾');
+        table.put('ض','⠫');
+        table.put('ص','⠯');
+        table.put('ش','⠩');
+        table.put('س','⠎');
+        table.put('ز' ,'⠵');
+        table.put('ر','⠗');
+        table.put('ذ','⠮');
+        table.put('د','⠙');
+        table.put('خ','⠭');
+        table.put('ح','⠱');
+        table.put('ج','⠚');
+        table.put('ث','⠹');
+        table.put('ت','⠞');
+        table.put('ب','⠃');
+        table.put('ا','⠁');
+        table.put('َ','⠂');
+        table.put('ِ','⠑');
+        table.put('ُ','⠥');
+        table.put('ً','⠆');
+        table.put('ٍ','⠔');
+        table.put('ٌ','⠢');
+        table.put('ّ','⠠');
+        table.put('ْ','⠒');
+        table.put('.','⠲');
+        table.put('،','⠐');
+    }
+}
