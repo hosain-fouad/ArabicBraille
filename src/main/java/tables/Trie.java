378b9a64ff003bff2d3aa0b2b11a35879968cc04
hosain-fathelbab <hosain.fathelbab@gmail.com>
2015-04-26 23:59:08 -0700

    handled tasheel with Grade2

--- src/main/java/tables/Trie.java
+++ src/main/java/tables/Trie.java
@@ -1,5 +1,7 @@
 package tables;
 
+import utils.grade2.Constants;
+
 /**
  * Created by hosainfathelbab on 4/6/15.
  */
@@ -43,6 +45,28 @@ public class Trie {
         TrieCell currentCell = root;
         for (int i = index; i < chars.length; i++) {
             char currentChar = chars[i];
+            if (currentCell.hasChild(currentChar)) {
+                currentCell = currentCell.getChildren().get(currentChar);
+                if (currentCell.hasAbbreviation()) {
+                    longestAbbreviation = currentCell.getAbbreviation();
+                }
+            }
+            else{
+                break;
+            }
+        }
+        return longestAbbreviation;
+    }
+
+    public Grade2Abbreviation getLongestMatchAbbreviationWithTashkeel(String input, int index) {
+        char[] chars = input.toCharArray();
+        Grade2Abbreviation longestAbbreviation = null;
+        TrieCell currentCell = root;
+        for (int i = index; i < chars.length; i++) {
+            char currentChar = chars[i];
+            if(Constants.tashkeel.contains(currentChar)) {
+                continue;
+            }
             if (currentCell.hasChild(currentChar)) {
                 currentCell = currentCell.getChildren().get(currentChar);
                 if (currentCell.hasAbbreviation()) {
--- src/main/java/translators/Grade2Translator.java
+++ src/main/java/translators/Grade2Translator.java
@@ -6,14 +6,16 @@ import rules.Rule;
 import tables.Grade1Map;
 import tables.Grade2Abbreviation;
 import tables.Trie;
+import utils.grade2.Constants;
 import utils.grade2.WordIterator;
+
 import java.util.Iterator;
 
 /**
  * Created by hosainfathelbab on 4/4/15.
  */
 @Repository(value = "grade2Translator")
-public class Grade2Translator extends Translator{
+public class Grade2Translator extends Translator {
 
     @Autowired
     private Translator grade1Translator;
@@ -24,30 +26,31 @@ public class Grade2Translator extends Translator{
         StringBuffer sb = new StringBuffer();
 
         Iterator<String> wordIterator = new WordIterator(input);
-        while(wordIterator.hasNext()){
+        while (wordIterator.hasNext()) {
             String word = wordIterator.next();
-            if(word.endsWith("ما") || word.endsWith("لم")) {
-                if(wordIterator.hasNext()){
+            if (word.endsWith("ما") || word.endsWith("لم")) {
+                if (wordIterator.hasNext()) {
                     word += wordIterator.next();
-                    if(wordIterator.hasNext()){
+                    if (wordIterator.hasNext()) {
                         word += wordIterator.next();
                     }
                 }
             }
-            sb.append(translateWord(word));
+            sb.append(translateWordWithTashkeel(word));
         }
 
         return sb.toString();
     }
 
-    private String translateWord(String word){
+    @Deprecated
+    private String translateWord(String word) {
         StringBuffer sb = new StringBuffer();
         int index = 0;
         while (index < word.length()) {
 
-            if(isNumber(word.charAt(index))){
+            if (isNumber(word.charAt(index))) {
                 sb.append("⠼");
-                while(index < word.length() && isNumber(word.charAt(index))){
+                while (index < word.length() && isNumber(word.charAt(index))) {
                     sb.append(Grade1Map.table.get(word.charAt(index++)));
                 }
                 continue;
@@ -56,7 +59,7 @@ public class Grade2Translator extends Translator{
             Grade2Abbreviation abbreviation = Trie.getInstance().getLongestMatchAbbreviation(word, index);
             if (abbreviation != null) {
                 boolean success = true;
-                if(abbreviation.getRules().size() > 0) {
+                if (abbreviation.getRules().size() > 0) {
                     success = false;
                     for (Rule rule : abbreviation.getRules()) {
                         if (rule.isValid(word, abbreviation.getWord(), index, sb.toString())) {
@@ -66,7 +69,7 @@ public class Grade2Translator extends Translator{
                     }
                 }
                 if (success) {
-                    if(abbreviation.getWord().length() > 2 && (index > 0 || index+abbreviation.getWord().length() < word.length()) && !abbreviation.getWord().equals("كان")) {
+                    if (abbreviation.getWord().length() > 2 && (index > 0 || index + abbreviation.getWord().length() < word.length()) && !abbreviation.getWord().equals("كان")) {
                         sb.append("⠤");
                     }
                     sb.append(abbreviation.getSymbol());
@@ -81,12 +84,67 @@ public class Grade2Translator extends Translator{
         return sb.toString();
     }
 
-    private boolean isNumber(char c){
-        if((c>='0'&&c<='9') || (c>='٠'&&c<='٩')) {
-            return true;
+    private String translateWordWithTashkeel(String word) {
+        StringBuffer sb = new StringBuffer();
+        int index = 0;
+        while (index < word.length()) {
+
+            if (isNumber(word.charAt(index))) {
+                sb.append("⠼");
+                while (index < word.length() && isNumber(word.charAt(index))) {
+                    sb.append(Grade1Map.table.get(word.charAt(index++)));
+                }
+                continue;
+            }
+
+            Grade2Abbreviation abbreviation = Trie.getInstance().getLongestMatchAbbreviationWithTashkeel(word, index);
+            if (abbreviation != null) {
+                String abbreviatedWordWithTashkeel = extractAbbreviationWithTashkeel(abbreviation, word, index);
+                boolean success = true;
+                if (abbreviation.getRules().size() > 0) {
+                    success = false;
+                    for (Rule rule : abbreviation.getRules()) {
+                        if (rule.isValid(word, abbreviatedWordWithTashkeel, index, sb.toString())) {
+                            success = true;
+                            break;
+                        }
+                    }
+                }
+                if (success) {
+                    if (abbreviation.getWord().length() > 2 && (index > 0 || index + abbreviatedWordWithTashkeel.length() < word.length()) && !abbreviation.getWord().equals("كان")) {
+                        sb.append("⠤");
+                    }
+                    sb.append(abbreviation.getSymbol());
+
+                    index = index + abbreviatedWordWithTashkeel.length();
+                    continue;
+                }
+            }
+            sb.append(grade1Translator.translate(word.charAt(index) + ""));
+            index++;
         }
-        else{
+        return sb.toString();
+    }
+
+    private boolean isNumber(char c) {
+        if ((c >= '0' && c <= '9') || (c >= '٠' && c <= '٩')) {
+            return true;
+        } else {
             return false;
         }
+    }
+
+    private String extractAbbreviationWithTashkeel(Grade2Abbreviation abbreviation, String word, int startIndex) {
+        int endIndex = startIndex;
+        for (char c : abbreviation.getWord().toCharArray()) {
+            while (c != word.charAt(endIndex)) {
+                endIndex++;
+            }
+            endIndex++;
+        }
+        if (endIndex < word.length() - 1 && Constants.tashkeel.contains(word.charAt(endIndex))) {
+            endIndex++;
+        }
+        return word.substring(startIndex, endIndex);
     }
 }
